2be2c6c70b0b3e9de1a9e515165893d41e07b427
erika <98649030+erikapelikh@users.noreply.github.com>
2022-01-29 22:07:08 +0300

    fv

---
+++ lab2/1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class lab2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int num = in.nextInt();
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        { factorial *= i;}
+        System.out.printf("Факториал %d = %d", num," = ",factorial);
+    }
+}
+
